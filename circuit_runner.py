a0e2361b06058c908822ba9cbe7b39871ec70701
Luc Kusters <ljbkusters@gmail.com>
2022-06-15 16:53:28 +0200

    circuit_runner: Add ImprovedRunner
    Add the class ImprovedRunner which aims to imporove circuit runner
    output from the main pecos circuit_runner. To this end the data
    structures MeasurementContainer and Measurement, both inheriting from
    dicts are added.
    
    The MeasurementContainer is a dict with added methods:
    - MeasurementContainer.to_list()
        returns a list of the values
    - MeasurementContainer.simplified()
        compatibility alias for to_list
    
    The Measurement class also inherits from a dict. It stores a single
    measurement. It has the added functionality of being able to return the
    syndrome of a measurement as a list of bits (0 or 1). The length of the
    list is determined by the num_qubits attribute, which is assigned at
    initialization. This num_qubits should be derived from the simulator
    state. The syndrome can be returned with the Measurement.syndrome()
    method.
    
    The ImprovedRunner wraps the run function to improve runner output to
    give more consistent results. The standard runner returns an empty set
    if no measurements were taken or if a measurement result was 0. This is
    confusing. This also goes for faults. The ImprovedRunner aims to improve
    the output by giving clearer output. If no measurement was taken or no
    faults were added, the runner now returns None in each respective case.
    If a measurement was taken, the runner now always returns a
    MeasurementContainer containing Measurements at specific ticks (as a
    value key pair). Measurements with a 0 measurement result simply store a
    0 as their value.

---
+++ circuit_runner.py
@@ -0,0 +1,101 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Runners.py
+@author Luc Kusters
+@date 02-06-2022
+"""
+
+import collections
+import copy
+import numpy.random
+
+import pecos.circuit_runners
+
+
+RunnerResult = collections.namedtuple("RunnerResult", ("state", "measurements",
+                                                       "faults"))
+
+
+class MeasurementContainer(dict):
+    """Data structure for containing measurements used in the ImprovedRunner
+
+    MeasurementContainer is a basic dict with some extra functionality
+    """
+
+    def ticks(self):
+        """get a list of keys"""
+        return list(self.keys())
+
+    def to_list(self):
+        """returns a list of measurements (without the keys)"""
+        return list(self.values())
+
+    def simplified(self):
+        """Compatibility alias, the method to_list is prefered"""
+        return self.to_list()
+
+    def first(self):
+        """Get the first measurement"""
+        return self.to_list()[0]
+
+    def last(self):
+        """Get the last measurement"""
+        return self.to_list()[-1]
+
+
+class Measurement(dict):
+    """Measurement object to store measurements made on a single tick"""
+
+    def __init__(self, num_qubits, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.num_qubits = num_qubits
+
+    @property
+    def syndrome(self):
+        """Measurement syndrome from the state it was measured on
+
+        Returns:
+            list of zeros and ones
+        """
+        return [self[i] if i in self.keys() else 0
+                for i in range(self.num_qubits)]
+
+    def __repr__(self):
+        return f"Measurement({super().__repr__()})"
+
+
+class ImprovedRunner(pecos.circuit_runners.Standard):
+    """Wrapper around standard runner, improves measurement output
+
+    If no measurements took place the measurement output is set to None
+    (similar for faults)
+
+    Measurement output is always given as a dict of ticks containing
+    dicts of locations with measurement output as a 0 or 1 (instead
+    of only showing measurment output IF the output is a 1)
+    """
+    MEASUREMENTS = ("measure X", "measure Y", "measure Z")
+
+    def __init__(self, random_seed=True, *args, **kwargs):
+        seed = numpy.random.randint(1e9) if random_seed else None
+        super().__init__(seed=seed, *args, **kwargs)
+
+    def run(self, state, circ, copy_state=False, *args, **kwargs):
+        if copy_state:
+            state = copy.deepcopy(state)
+        std_meas, std_faults = super().run(state, circ, *args, **kwargs)
+        meas = MeasurementContainer()
+        for i, item in enumerate(circ.items()):
+            if (any(meas_gate in item for meas_gate in self.MEASUREMENTS)):
+                meas[i] = Measurement(num_qubits=state.num_qubits)
+                locations = item[1]
+                locations_meas_ones = std_meas[i] if len(std_meas) > 0 else []
+                for loc in locations:
+                    meas[i][loc] = 1 if loc in locations_meas_ones else 0
+        if len(meas) == 0:
+            meas = None
+        faults = std_faults
+        if len(std_faults) == 0:
+            faults = None
+        return RunnerResult(state, meas, faults)
