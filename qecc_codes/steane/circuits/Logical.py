9fcdf59289f5a21dd8bf2d2cf6bb07c92e6fbfc9
Luc Kusters <ljbkusters@gmail.com>
2022-06-16 12:05:33 +0200

    Steane: Add Logical
    Add some logical circuits

---
+++ qecc_codes/steane/circuits/Logical.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Initialization.py
+@author Luc Kusters
+@date 06-06-2022
+"""
+
+
+from toolkits.qecc_codes.steane.circuits import Steane
+from toolkits.qecc_codes.steane.circuits import Measurement
+
+
+class LogicalZeroInitialization(Steane.BaseSteaneCirc):
+    """
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.append("init |0>", set(self.DATA_QUBITS))
+        self.append("H", {self.CORNER_TOP_QUBIT})
+        self.update("H", {self.CORNER_LEFT_QUBIT})
+        self.update("H", {self.CORNER_RIGHT_QUBIT})
+        self.append("CNOT", {(self.CORNER_TOP_QUBIT, self.EDGE_LEFT_QUBIT)})
+        self.append("CNOT", {(self.CORNER_TOP_QUBIT, self.CENTER_QUBIT)})
+        self.append("CNOT", {(self.CORNER_TOP_QUBIT, self.EDGE_RIGHT_QUBIT)})
+        self.append("CNOT", {(self.CORNER_LEFT_QUBIT, self.EDGE_LEFT_QUBIT)})
+        self.append("CNOT", {(self.CORNER_LEFT_QUBIT, self.CENTER_QUBIT)})
+        self.append("CNOT", {(self.CORNER_LEFT_QUBIT, self.EDGE_BOTTOM_QUBIT)})
+        self.append("CNOT", {(self.CORNER_RIGHT_QUBIT,
+                              self.EDGE_BOTTOM_QUBIT)})
+        self.append("CNOT", {(self.CORNER_RIGHT_QUBIT, self.CENTER_QUBIT)})
+        self.append("CNOT", {(self.CORNER_RIGHT_QUBIT, self.EDGE_RIGHT_QUBIT)})
+
+
+class LogicalPauli(Steane.BaseSteaneCirc):
+    LOGICALS = {
+            "edge_left": (0, 1, 4),
+            "edge_right": (0, 3, 6),
+            "edge_bottom": (4, 5, 6),
+            "center_vertical": (0, 2, 5),
+            "center_left": (4, 2, 3),
+            "center_right": (6, 2, 1),
+            }
+
+    def __init__(self, pauli_type, logical="edge_left", *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if logical == "all":
+            self.append("pauli_type", set(self.LOGICALS[logical]))
