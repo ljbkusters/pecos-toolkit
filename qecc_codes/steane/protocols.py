b8c16a8df2f438ccad6c0203b15af981bfc7c240
Luc Kusters <ljbkusters@gmail.com>
2022-06-20 16:58:35 +0200

    Steane.protocols: Overhaul f1ftec protocol
    Overhaul of large parts of the f1ftec protocol after more research into
    the topic showed that the currently implemented protocol did not
    implement the F1FTEC protocol properly.

--- qecc_codes/steane/protocols.py
+++ qecc_codes/steane/protocols.py
@@ -31,9 +31,9 @@ class SteaneProtocol(object):
         return RUNNER.run(circ.simulator(), circ, *args, **kwargs)
 
     @staticmethod
-    def idle_data_qubits(*args, **kwargs):
+    def idle_data_qubits(state, *args, **kwargs):
         circ = Steane.IdleDataBlock()
-        return RUNNER.run(circ.simulator(), circ, *args, **kwargs)
+        return RUNNER.run(state, circ, *args, **kwargs)
 
     @staticmethod
     def measure_stabilizers(state, stabilizers, *args, **kwargs):
@@ -77,9 +77,8 @@ class SteaneProtocol(object):
             circ = Steane.SingleQubitPauli(corr_pauli_type, corr_qubit)
         else:
             circ = Steane.BaseSteaneCirc()
-            # "I", Steane.BaseSteaneCirc.set_of_qubits)
-        state, meas, fault = RUNNER.run(state, circ, *args, **kwargs)
-        return state, meas, fault
+        res = RUNNER.run(state, circ, *args, **kwargs)
+        return res
 
     @staticmethod
     def x_steane_round(state, *args, **kwargs):
@@ -183,106 +182,69 @@ class F1FTECProtocol(object):
         pass
 
     @staticmethod
-    def flag_measure_stabilizers(state, stabilizers, *args, **kwargs):
-        """Measure a set of stabilizers on a given state
-
-        Args:
-            state: pecos simulator state
-            stabilizers: iterable of stabilizers
-            *args, **kwargs passed to RUNNER.run()
-        Returns:
-            list of measurements (measurement qubit only)
-            list of faults (if error generator was used)
-        """
-        syndromes = []
-        flags = []
-        faults = []
-        for stab in stabilizers:
-            circ = Measurement.F1FTECStabMeasCircuit(stab)
-            res = RUNNER.run(state, circ, *args, **kwargs)
-            print(res.measurements)
-            print(type(res.measurements))
-            syndromes.append(res.measurements.simplified().syndrome[circ.ANCILLA_QUBIT])
-            flags.append(res.measurements.simplified().syndrome[circ.FLAG_QUBIT])
-            faults.append(res.faults)
-        syndrome = Syndrome.Syndrome(stabilizers[0].pauli_type, *syndromes)
-        flag_syndrome = Syndrome.Syndrome("flag", *syndromes)
-        return F1FTECProtocol.SyndromeMeasResults(syndrome, flag_syndrome,
-                                                  faults)
+    def flag_measure_stabilizer(state, stab, *args, **kwargs):
+        circ = Measurement.F1FTECStabMeasCircuit(stab)
+        return RUNNER.run(state, circ, *args, **kwargs)
 
     @staticmethod
-    def flag_measure_x_stabilizers(state, *args, **kwargs):
-        return F1FTECProtocol.flag_measure_stabilizers(
-                state, Steane.BaseSteaneData.x_stabilizers, *args, **kwargs)
-
-    @staticmethod
-    def flag_measure_z_stabilizers(state, *args, **kwargs):
-        return F1FTECProtocol.flag_measure_stabilizers(
-                state, Steane.BaseSteaneData.z_stabilizers, *args, **kwargs)
-
-    @staticmethod
-    def flag_measure_all_stabilizers(state, *args, **kwargs):
-        x_meas_res = F1FTECProtocol.flag_measure_x_stabilizers(
-                state, *args, **kwargs)
-        z_meas_res = F1FTECProtocol.flag_measure_z_stabilizers(
-                state, *args, **kwargs)
-        return x_meas_res, z_meas_res
-
-    @staticmethod
-    def flag_correct_from_syndrome():
-        pass
-
-    @staticmethod
-    def is_flagged(meas_res):
-        return any(val == 1 for val in meas_res.flag_syndrome)
-
-    @staticmethod
-    def f1ftec_on_flag_action(state, steane_meas_f, *args, **kwargs):
-        res = steane_meas_f(state, *args, **kwargs)
-        syndrome = res.syndrome
-
-
-    @staticmethod
-    def f1ftec_round(state, meas_f, *args, **kwargs):
-        if meas_f == F1FTECProtocol.flag_measure_x_stabilizers:
-            steane_meas_f = SteaneProtocol.measure_x_stabilizers
-        elif meas_f == F1FTECProtocol.flag_measure_z_stabilizers:
-            steane_meas_f = SteaneProtocol.measure_z_stabilizers
+    def f1ftec_round(state, *args, **kwargs):
+        syndrome = {}
+        ancilla_qubit = Measurement.F1FTECStabMeasCircuitData.ANCILLA_QUBIT
+        x_stabs = Steane.BaseSteaneData.x_stabilizers
+        z_stabs = Steane.BaseSteaneData.z_stabilizers
+        for pauli_type, stabs in {"X": x_stabs, "Z": z_stabs}.items():
+            syndrome[pauli_type] = []
+            for i in range(2):
+                syndrome[pauli_type].append([])
+                for stab in stabs:
+                    res = F1FTECProtocol.flag_measure_stabilizer(
+                        state, stab, *args, **kwargs)
+                    bit = res.measurements.last().syndrome[ancilla_qubit]
+                    syndrome[pauli_type][i].append(bit)
+                    # if a flag occurs, stop and do non-FT meas + modified
+                    # correction based on which circuit (stab) flagged
+                    if F1FTECProtocol.is_flagged(res):
+                        print("detected flag")
+                        return F1FTECProtocol.correct_from_flagged_circuit(
+                                state, stab, *args, **kwargs)
+                syndrome[pauli_type][i] = \
+                    Syndrome.Syndrome(pauli_type, *syndrome[pauli_type][i])
+        # no flags occured, check if syndromes were different
+        # if so, measure non-FT and correct with normal steane correction
+        print(syndrome)
+        if syndrome[pauli_type][0] != syndrome[pauli_type][1]:
+            print("no flag, syndromes don't match")
+            return F1FTECProtocol.correct_non_flagged_erred_circuit(
+                    state, stabs, *args, **kwargs)
+        # if not, do standard correction on the syndrome
         else:
-            raise TypeError("meas_f has to be one of (SteaneProtocol."
-                            "measure_x_stabilizers, SteaneProtocol."
-                            "measure_z_stabilizers)")
-        meas_res_1 = meas_f(state, *args, **kwargs)
-        if F1FTECProtocol.is_flagged(meas_res_1):
-            return F1FTECProtocol.f1ftec_on_flag_action(
-                    state, steane_meas_f, *args, **kwargs)
-        meas_res_2 = meas_f(state, *args, **kwargs)
-        if F1FTECProtocol.is_flagged(meas_res_2):
-            return F1FTECProtocol.f1ftec_on_flag_action(
-                    state, steane_meas_f, *args, **kwargs)
-        # no flags occured
-        if meas_res_1.syndrome == meas_res_2.syndrome:
-            # correct E(s)
-            SteaneProtocol.correct_from_syndrome(state, meas_res_1.syndrome,
-                                                 *args, **kwargs)
+            print("no flag, syndromes match")
+            return SteaneProtocol.correct_from_syndrome(
+                    state, syndrome[pauli_type][0], *args, **kwargs)
+
+    @staticmethod
+    def correct_from_flagged_circuit(state, stabs, stab, *args, **kwargs):
+        decoder = Syndrome.FlaggedSyndromeDecoder(stab)
+        syndrome = SteaneProtocol.measure_stabilizers(
+                state, stabs, *args, **kwargs)
+        corr_qubits, corr_pauli_type = decoder.lot_decoder(syndrome)
+        circ = Steane.BaseSteaneCirc()
+        if corr_qubits is not None:
+            circ.append(corr_pauli_type, set(corr_qubits))
+        return RUNNER.run(state, circ, *args, **kwargs)
+
+    @staticmethod
+    def correct_non_flagged_erred_circuit(state, stabs, *args, **kwargs):
+        if stabs[0].pauli_type == "X":
+            return SteaneProtocol.x_steane_round(state, *args, **kwargs)
+        if stabs[0].pauli_type == "Z":
+            return SteaneProtocol.z_steane_round(state, *args, **kwargs)
         else:
-            # measure once more non-FT and correct E(s) (i.e. Steane round)
-            SteaneProtocol.x_steane_round(state, *args, **kwargs)
+            raise ValueError("pauli type could not be identified in erred"
+                             "f1ftec non-flagged syndrome")
 
     @staticmethod
-    def f1ftec_x_round(state, *args, **kwargs):
-        return F1FTECProtocol.f1ftec_round(
-                state, meas_f=F1FTECProtocol.flag_measure_x_stabilizers,
-                *args, **kwargs)
-
-    @staticmethod
-    def f1ftec_z_round(state, *args, **kwargs):
-        return F1FTECProtocol.f1ftec_round(
-                state, meas_f=F1FTECProtocol.flag_measure_x_stabilizers,
-                *args, **kwargs)
-
-    @staticmethod
-    def full_f1ftec_round(state, *args, **kwargs):
-        x_res = F1FTECProtocol.f1ftec_x_round(state, *args, **kwargs)
-        z_res = F1FTECProtocol.f1ftec_z_round(state, *args, **kwargs)
-        return x_res, z_res
+    def is_flagged(res):
+        flag_qubit = Measurement.F1FTECStabMeasCircuitData.FLAG_QUBIT
+        flag = res.measurements.last().syndrome[flag_qubit]
+        return bool(flag)
