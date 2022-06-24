f897076e616867241cad5dbca7f5a55c1ffa74ec
Luc Kusters <ljbkusters@gmail.com>
2022-06-24 11:26:05 +0200

    Make package installable

---
+++ setup.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+setup.py
+@author Luc Kusters
+@date 24-06-2022
+"""
+
+from setuptools import setup
+
+setup(name='pecos_toolkit',
+      version='0.0.1',
+      description='Toolkit with useful implemented code based on pecos module',
+      url='https://ljbkusters.eu',
+      author='Luc Kusters',
+      author_email='luc.kusters@rwth-aachen.de',
+      license='GNU GPLv3',
+      packages=['pecos_toolkit'],
+      zip_safe=False)
