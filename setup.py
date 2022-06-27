c45c9bdddcfc79f05601f81c52c115dffd5c239d
Luc Kusters <ljbkusters@gmail.com>
2022-06-27 11:04:11 +0200

    Fix setup.py and add __init__.py files
    Fix setup.py. Now recognizes all subpackages. Also add numpy dependency.
    
    __init__.py files help recognize subpackages during setup and
    installation of the module.

--- setup.py
+++ setup.py
@@ -6,14 +6,27 @@ setup.py
 @date 24-06-2022
 """
 
+import pip
 from setuptools import setup
+from setuptools import find_packages
+
+# installed = pip.freeze()
+# if "quantum-pecos==2.0.dev0" not in installed:
+#     print("This install relies on the RWTH Quantum-PECOS 2.0.dev0"
+#           " distribution. as it is currently not publicly available, please"
+#           " install this package by hand!")
 
 setup(name='pecos_toolkit',
       version='0.0.1',
       description='Toolkit with useful implemented code based on pecos module',
-      url='https://ljbkusters.eu',
+      url='https://git.rwth-aachen.de/masters-thesis/code/pecos-toolkit',
       author='Luc Kusters',
       author_email='luc.kusters@rwth-aachen.de',
       license='GNU GPLv3',
-      packages=['pecos_toolkit'],
-      zip_safe=False)
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=[
+            "numpy",
+            # "pecos==0.2.dev0",
+          ],
+      )
