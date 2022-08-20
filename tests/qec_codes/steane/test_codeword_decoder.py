bacc01ff7ace32df4965a94450d1c947a4161c6d
Luc Kusters <ljbkusters@gmail.com>
2022-08-20 09:47:43 +0200

    tests: Rework codeword_decoder test to unittest

--- tests/qec_codes/steane/test_codeword_decoder.py
+++ tests/qec_codes/steane/test_codeword_decoder.py
@@ -7,53 +7,78 @@ test_codeword_decoder.py
 """
 
 import copy
+import unittest
 
 from pecos_toolkit.qecc_codes.steane import protocols
+import testsuite
 
-zero_state = protocols.SteaneProtocol.init_logical_zero().state
-one_state = copy.deepcopy(zero_state)
-one_state.run_gate("X", {0, 1, 4})
 
-N = 5
+class TestCodeWordDecoder(testsuite.LoggedTestCase):
 
-print(f"testing {N} runs of decoding perfect zero states")
-for i in range(N):
-    print("zero state",
-          protocols.SteaneProtocol.decode_state(zero_state, copy_state=True))
+    @classmethod
+    def tearDownClass(cls):
+        pass
 
-print(f"testing {N} runs of decoding perfect one states")
-for i in range(N):
-    print("one state",
-          protocols.SteaneProtocol.decode_state(one_state, copy_state=True))
+    def setUp(self):
+        self.logger.debug("Initializing a steane logical zero and one state")
+        self.zero_state = protocols.SteaneProtocol.init_logical_zero().state
+        self.one_state = copy.deepcopy(self.zero_state)
+        self.one_state.run_gate("X", {0, 1, 4})
+        self.num_reps = 5
 
-erred_zero = copy.deepcopy(zero_state)
-erred_zero.run_gate("X", {0})
-erred_one = copy.deepcopy(one_state)
-erred_one.run_gate("X", {0})
+    def tearDown(self):
+        pass
 
-print(f"testing {N} runs of decoding zero states with wt 1 error")
-for i in range(N):
-    print("zero state",
-          protocols.SteaneProtocol.decode_state(erred_zero, copy_state=True))
+    def check_parity(self, state, expected):
+        parity = protocols.SteaneProtocol.decode_state(
+                  state, copy_state=True)
+        self.logger.debug(f"Logical parity: {parity}")
+        self.assertEqual(parity, expected)
 
-print(f"testing {N} runs of decoding one states with wt 1 error")
-for i in range(N):
-    print("one state",
-          protocols.SteaneProtocol.decode_state(erred_one, copy_state=True))
+    def check_error(self, state, error, expected_parity):
+        state = copy.deepcopy(state)
+        state.run_gate("X", set(error))
+        self.check_parity(state, expected_parity)
 
-logical_erred_zero = copy.deepcopy(erred_zero)
-logical_erred_zero.run_gate("X", {1})
-logical_erred_one = copy.deepcopy(erred_one)
-logical_erred_one.run_gate("X", {1})
+    def test_perfect_codewords(self):
+        self.logger.debug(f"testing {self.num_reps} runs of decoding perfect"
+                          " zero states")
+        self.logger.debug(f"testing {self.num_reps} runs of decoding perfect"
+                          " one states")
+        for i in range(self.num_reps):
+            self.check_parity(self.zero_state, 0)
+            self.check_parity(self.one_state, 1)
 
-print(f"testing {N} runs of decoding zero states with wt 2 error")
-for i in range(N):
-    print("zero state",
-          protocols.SteaneProtocol.decode_state(logical_erred_zero,
-                                                copy_state=True))
+    def test_wt1_erred_codewords(self):
+        self.logger.debug("Testing weight one erred states")
+        for i in range(self.num_reps):
+            self.check_error(self.zero_state, {0}, 0)
+            self.check_error(self.one_state, {0}, 1)
 
-print(f"testing {N} runs of decoding one states with wt 2 error")
-for i in range(N):
-    print("one state",
-          protocols.SteaneProtocol.decode_state(logical_erred_one,
-                                                copy_state=True))
+    def test_wt2_erred_codewords(self):
+        self.logger.debug("Testing weight 2 erred states")
+        for i in range(self.num_reps):
+            self.check_error(self.zero_state, {0, 1}, 1)
+            self.check_error(self.one_state, {0, 1}, 0)
+
+    def test_wt3_logical_erred_codewords(self):
+        self.logger.debug("Testing weight 2 erred states")
+        for i in range(self.num_reps):
+            self.check_error(self.zero_state, {0, 1, 4}, 1)
+            self.check_error(self.one_state, {0, 1, 4}, 0)
+
+    def test_wt3_correctible_erred_codewords(self):
+        self.logger.debug("Testing weight 2 erred states")
+        for i in range(self.num_reps):
+            self.check_error(self.zero_state, {0, 1, 2}, 0)
+            self.check_error(self.one_state, {0, 1, 2}, 1)
+
+    def test_wt4_correctible_erred_codewords(self):
+        self.logger.debug("Testing weight 2 erred states")
+        for i in range(self.num_reps):
+            self.check_error(self.zero_state, {0, 1, 2, 3}, 0)
+            self.check_error(self.one_state, {0, 1, 2, 3}, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
