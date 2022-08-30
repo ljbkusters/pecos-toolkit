3f552fa1d5c96c38ba06c99a34cd0f7816a3a3b5
Luc Kusters <ljbkusters@gmail.com>
2022-08-30 17:39:33 +0200

    qecc_codes.steane.decoders: Add Sequential LOT Decoder
    Also moves previous decoders to the decoders directory. This change
    still has to be permeated through to other parts of the module that
    directly use the decoder, such that all decoders are initialized from
    the decoders module instead of data types (where it shouldn't have been
    in the first place).

---
+++ pecos_toolkit/qecc_codes/steane/decoders/BasicLOTDecoder.py
@@ -0,0 +1,90 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Syndrome.py
+@author Luc Kusters
+@date 07-06-2022
+"""
+
+import collections
+
+Syndrome = collections.namedtuple("Syndrome", "syndrome_type top left right")
+XZSyndrome = collections.namedtuple("XZSyndrome", "x_syndrome z_syndrome")
+XZSyndrome = collections.namedtuple("XZSyndrome", "x_syndrome z_syndrome")
+
+
+class SteaneSyndromeDecoder(object):
+
+    LOT = {"000": None,
+           "001": 6,
+           "010": 4,
+           "011": 5,
+           "100": 0,
+           "101": 3,
+           "110": 1,
+           "111": 2,
+           }
+
+    @staticmethod
+    def bits_to_lot_key(top, left, right):
+        return "{:}{:}{:}".format(top, left, right)
+
+    def syndrome_to_lot_key(self, syndrome: Syndrome):
+        return self.bits_to_lot_key(syndrome.top,
+                                    syndrome.left,
+                                    syndrome.right)
+
+    @staticmethod
+    def pauli_correction_type(syndrome: Syndrome):
+        if syndrome.syndrome_type == "X":
+            return "Z"
+        elif syndrome.syndrome_type == "Z":
+            return "X"
+        elif syndrome.syndrome_type == "flag":
+            raise ValueError("'flag' syndromes have to be decoded using the"
+                             " FlagSyndromeDecoder.")
+        else:
+            raise ValueError("Unknown syndrome.syndrome_type: no corrections"
+                             " specified. syndrome_type must be one of"
+                             " ('X', 'Z') but syndrome.syndrome_type was"
+                             f" {syndrome.syndrome_type}")
+
+    def lot_decoder(self, syndrome: Syndrome):
+        corr_type = self.pauli_correction_type(syndrome)
+        return self.cor_qubit(syndrome), corr_type
+
+    def cor_qubit(self, syndrome: Syndrome):
+        lot_key = self.syndrome_to_lot_key(syndrome)
+        return self.LOT[lot_key]
+
+    def classical_lot_decoder(self, top, left, right):
+        lot_key = self.bits_to_lot_key(top, left, right)
+        return self.LOT[lot_key]
+
+
+class FlaggedSyndromeDecoder(SteaneSyndromeDecoder):
+
+    KEY_FROM_CORRECTION = {
+            (2, 3): "010",
+            (3, 2): "010",
+            (1, 2): "001",
+            (2, 1): "001",
+            (2, 5): "100",
+            (5, 2): "100",
+            }
+
+    def __init__(self, stab, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.LOT = {"000": None,
+                    "001": (6, ),
+                    "010": (4, ),
+                    "011": (5, ),
+                    "100": (0, ),
+                    "101": (3, ),
+                    "110": (1, ),
+                    "111": (2, ),
+                    }
+
+        correction = stab.qubits[2:4]
+        key = self.KEY_FROM_CORRECTION[correction]
+        self.LOT[key] = correction
---
+++ pecos_toolkit/qecc_codes/steane/decoders/SequentialLOTDecoder.py
@@ -0,0 +1,111 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Syndrome.py
+@author Luc Kusters
+@date 07-06-2022
+"""
+
+import collections
+import numpy
+
+from pecos_toolkit.qecc_codes.steane.circuits import Steane
+from pecos_toolkit.qecc_codes.steane.data_types import RNNDataTypes
+from pecos_toolkit.qecc_codes.steane.data_types import Syndrome
+from pecos_toolkit.qecc_codes.steane.data_types import Plaquette
+from pecos_toolkit.qecc_codes.steane.decoders import BasicLOTDecoder
+
+
+class SequentialLOTDecoder(object):
+
+    CODE_DATA = Steane.BaseSteaneData
+
+    def __init__(self):
+        self.basic_lot_decoder = BasicLOTDecoder.SteaneSyndromeDecoder()
+
+    def decode_sequence(self, sequence_data):
+        seq_data_len = len(sequence_data)
+        num_data_qubits = len(self.CODE_DATA.DATA_QUBITS)
+        corrections = numpy.zeros((seq_data_len, 2, num_data_qubits),
+                                  dtype=bool)
+        print(corrections)
+        for time_step in range(seq_data_len):
+            correction = self.correction_per_time_step(sequence_data,
+                                                       time_step)
+            corrections[time_step, :, :] = numpy.asarray(correction)
+        compiled_corrections = {
+                "X": self.compile_corrections(corrections[:, 0, :]),
+                "Z": self.compile_corrections(corrections[:, 1, :])
+            }
+        return compiled_corrections
+
+    def unpack_sequence_step(self, sequence_data, time_step):
+        """Unpack the sequence data for a time_step
+        """
+        if isinstance(sequence_data, RNNDataTypes.RNNSyndromeData):
+            return self.unpack_RNNSyndromeData(sequence_data, time_step)
+        elif isinstance(sequence_data, numpy.ndarray):
+            return self.unpack_vectorized_sequence(sequence_data, time_step)
+
+    def unpack_RNNSyndromeData(self, sequence_data, time_step):
+        return (sequence_data.x_syndrome_increments(time_step),
+                sequence_data.x_flags(time_step),
+                sequence_data.z_syndrome_increments(time_step),
+                sequence_data.z_flags(time_step))
+
+    def unpack_vectorized_sequence(self, sequence_data, time_step):
+        return (sequence_data[time_step][:3],
+                sequence_data[time_step][3:6],
+                sequence_data[time_step][6:9],
+                sequence_data[time_step][9:12])
+
+    def correction_per_time_step(self, sequence_data, time_step):
+        x_syninc, _, z_syninc, _ = \
+            self.unpack_sequence_step(sequence_data, time_step)
+        if time_step == 0:
+            x_flags_last = None
+            z_flags_last = None
+        else:
+            _, x_flags_last, _, z_flags_last = \
+                self.unpack_sequence_step(sequence_data, time_step - 1)
+        x_correction = self.correction_for_syndrome_data(x_syninc,
+                                                         z_flags_last)
+        z_correction = self.correction_for_syndrome_data(z_syninc,
+                                                         x_flags_last)
+        return x_correction, z_correction
+
+    def correction_for_syndrome_data(self, syninc, flags_last):
+        if flags_last is None or sum(flags_last) == 0 or sum(flags_last) > 1:
+            # return default decoder
+            decoder = self.basic_lot_decoder
+        elif sum(flags_last) == 1:
+            plaq_idx = numpy.where(numpy.asarray(flags_last) == 1)[0][0]
+            plaq = self.CODE_DATA.plaquettes[plaq_idx]
+            decoder = BasicLOTDecoder.FlaggedSyndromeDecoder(plaq)
+            # return default flagged decoder
+        else:
+            raise NotImplementedError("This should not occur!")
+        syndrome = Syndrome.Syndrome(None, *syninc)
+        qubits = decoder.cor_qubit(syndrome)
+        if not hasattr(qubits, "__iter__"):
+            qubits = (qubits,)
+        return self.qubits_to_correction_vector(qubits)
+
+    def compile_corrections(self, corrections):
+        corrections = numpy.sum(numpy.asarray(corrections), axis=0) % 2
+        return corrections
+
+    def correction_vector_to_qubits(self, correction_vector):
+        """return a tuple qubits on which a correction should be applied"""
+        x = numpy.where(numpy.asarray(correction_vector) == 1)[0]
+        return x
+
+    def qubits_to_correction_vector(self, qubits):
+        """return an array of bits representing if a correction on this
+        qubit must be applied"""
+        return numpy.fromiter(
+                (1 if i in qubits else 0
+                 for i in self.CODE_DATA.DATA_QUBITS), dtype=bool)
+
+    def correction_logical_weight(self, corrections):
+        pass
